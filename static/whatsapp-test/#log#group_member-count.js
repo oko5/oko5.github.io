81e3b543ee73d7480d1d378e67b1a5339bef59a5
oko5 <fxmail.0002@gmail.com>
2026-08-24 08:04:42 +0800

    Delete static/whatsapp-test/#log#group_member-count.js

--- static/whatsapp-test/#log#group_member-count.js
+++
@@ -1,6 +0,0 @@
-const button = [...document.querySelectorAll('button')]
-    .find(btn => /^\d+ members$/.test(btn.textContent.trim()));
-
-if (button) {
-    console.log(button.textContent.trim());
-}
